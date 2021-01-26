06c42ebe9d87a4814e5c75d0e3ce81079afa5c6a
Jill-Ann <jillannkay@gmail.com>
2021-01-26 21:36:14 +0100

    Add navbar spec

---
+++ cypress/integration/e2e-tests/navbar.spec.js
@@ -0,0 +1,24 @@
+describe('Navbar test', () => {
+  before(() => {
+      cy.visit('http://zero.webappsecurity.com/index.html')
+  })
+  
+  it('should display online banking content', () => {
+      cy.contains('Online Banking').click()
+    //   cy.get('#onlineBankingMenu').click()
+      cy.url().should('include', 'online-banking.html')
+      cy.get('h1').should('be.visible')
+    })
+    
+    it('should display feedback content', () => {
+      cy.contains('Feedback').click()
+      cy.url().should('include', 'feedback.html')
+      cy.get('#feedback-title').should('be.visible')
+    })
+    
+    it('should display homepage content', () => {
+      cy.contains('Zero Bank').click()
+      cy.url().should('include', 'index.html')
+      cy.get('h4').contains('Online Banking')
+  })
+})
